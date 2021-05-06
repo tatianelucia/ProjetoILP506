34d0528fb27c4715aaeaa0cdb0ff7a52e1600e73
Tatiane Lucia <tatiane.lucia1998@hotmail.com>
2021-05-06 17:51:00 -0300

    Subindo Atualizações - 06/05/2021

--- app/src/main/java/com/ilp506/conversao/Conversao.java
+++ app/src/main/java/com/ilp506/conversao/Conversao.java
@@ -1,8 +1,18 @@
 package com.ilp506.conversao;
 
+import android.view.View;
+
 public class Conversao {
 
-    public static double centimetrosPolegadas(double centimetros) {
+    public static double celsiusFahrenheit(double f){
+        return (f-32)/1.8;
+    }
+
+    public static double fahrenheitCelsius(double c){
+        return c*1.8 + 32;
+    }
+
+    /*public static double centimetrosPolegadas(double centimetros) {
 
     }
 
@@ -26,19 +36,11 @@ public class Conversao {
 
     }
 
-    public static double celsiusFahrenheit(double f){
-        return (f-32)*5/9;
-    }
-
-    public static double fahrenheitcelsius(double c){
-        return (32+c)*9/5;
-    }
-
     public static double nosKm(double km){
 
     }
 
     public static double kmNos(double nos){
 
-    }
+    } */
 }
