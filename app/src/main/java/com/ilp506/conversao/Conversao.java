2a17c8401a9f2620b035a547c645d4054ca6d8b8
Tatiane Lucia <tatiane.lucia1998@hotmail.com>
2021-05-06 15:35:15 -0300

    Subindo os métodos.

---
+++ app/src/main/java/com/ilp506/conversao/Conversao.java
@@ -0,0 +1,24 @@
+package com.ilp506.conversao;
+
+public class Conversao {
+
+    public double polegadasECentimetros(double polegadas, double centimetros) {
+
+    }
+
+    public double pesEMetros(double pes, double metros) {
+
+    }
+
+    public double milhaEKm(double milha, double km) {
+
+    }
+
+    public double celsiusEFahrenheit(double celsius, double fahrenheit){
+
+    }
+
+    public double nosEKm(double nos, double km){
+
+    }
+}
