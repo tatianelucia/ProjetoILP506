d9902ab00704b1d0e7ab9683f69aa67c0578c86f
Tatiane Lucia <tatiane.lucia1998@hotmail.com>
2021-05-15 18:13:08 -0300

    Subindo alterações.

--- app/src/main/java/com/ilp506/conversao/Conversao.java
+++ app/src/main/java/com/ilp506/conversao/Conversao.java
@@ -1,7 +1,10 @@
 package com.ilp506.conversao;
 
-import android.view.View;
+import android.icu.text.DecimalFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class Conversao {
 
     public static double celsiusFahrenheit(double c){
@@ -12,20 +15,42 @@ public class Conversao {
         return (f-32)/1.8;
     }
 
-    public static double centimetrosPolegadas(double centimetros){ return (centimetros/2.54); }
+    public static Object centimetrosPolegadas(double centimetros){
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        return formatador.format(centimetros/2.54);
+    }
 
-    public static double polegadasCentimetros(double polegadas) {return (polegadas*2.54); }
+    public static Object polegadasCentimetros(double polegadas) {
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        return formatador.format(polegadas*2.54);
+        }
 
+    public static Object pesMetros(double pes) {
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        return formatador.format(pes/3.2808);
+    }
 
-    public static double pesMetros(double pes) {return (pes/0.3048); }
+    public static Object metrosPes(double metros) {
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        return formatador.format(metros*3.2808);
+    }
 
-    public static double metrosPes(double metros) {return (metros*0.3048); }
+    public static Object milhaKm(double milha) {
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        return formatador.format(milha*1.609);
+    }
 
-    public static double milhaKm(double milha) {return (milha*1.609); }
+    public static Object kmMilha(double km) {
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        return formatador.format(km/1.609);  }
 
-    public static double kmMilha(double km) {return (km/1.609);  }
+    public static Object nosKm(double nos) {
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        return formatador.format(nos*1.852);
+    }
 
-    public static double nosKm(double nos){return (nos*1.852);   }
-
-    public static double kmNos(double km){return (km/1.852);    }
+    public static Object kmNos(double km) {
+        DecimalFormat formatador = new DecimalFormat("0.00");
+        return formatador.format(km/1.852);
+    }
 }
