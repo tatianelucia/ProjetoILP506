d0f733267a2501ceae639452246b3409b6137838
erich fratini <erichfratini@protonmail.com>
2021-05-15 16:43:47 -0300

    aplicação funcionando sem bugs

--- app/src/main/java/com/ilp506/conversao/Conversao.java
+++ app/src/main/java/com/ilp506/conversao/Conversao.java
@@ -4,43 +4,28 @@ import android.view.View;
 
 public class Conversao {
 
-    public static double celsiusFahrenheit(double f){
-        return (f-32)/1.8;
-    }
-
-    public static double fahrenheitCelsius(double c){
+    public static double celsiusFahrenheit(double c){
         return c*1.8 + 32;
     }
 
-    /*public static double centimetrosPolegadas(double centimetros) {
-
+    public static double fahrenheitCelsius(double f){
+        return (f-32)/1.8;
     }
 
-    public static double polegadasCentimetros(double polegadas) {
+    public static double centimetrosPolegadas(double centimetros){ return (centimetros/2.54); }
 
-    }
+    public static double polegadasCentimetros(double polegadas) {return (polegadas*2.54); }
 
-    public static double pesMetros(double pes) {
 
-    }
+    public static double pesMetros(double pes) {return (pes/0.3048); }
 
-    public static double metrosPes(double metros) {
+    public static double metrosPes(double metros) {return (metros*0.3048); }
 
-    }
+    public static double milhaKm(double milha) {return (milha*1.609); }
 
-    public static double milhaKm(double km) {
+    public static double kmMilha(double km) {return (km/1.609);  }
 
-    }
+    public static double nosKm(double nos){return (nos*1.852);   }
 
-    public static double kmMilha(double milha) {
-
-    }
-
-    public static double nosKm(double km){
-
-    }
-
-    public static double kmNos(double nos){
-
-    } */
+    public static double kmNos(double km){return (km/1.852);    }
 }
--- app/src/main/java/com/ilp506/conversao/MainActivity.java
+++ app/src/main/java/com/ilp506/conversao/MainActivity.java
@@ -1,26 +1,39 @@
 package com.ilp506.conversao;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    Spinner spinner;
+    TextView txtEntrada;
+    TextView txtSaida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner= findViewById(R.id.spConversor);
+        txtSaida = findViewById(R.id.txtSaida);
+        txtEntrada = findViewById(R.id.txtEntrada);
+        txtSaida.setEnabled(false);
+    }
 
-        Spinner spinner = findViewById(R.id.spConversor);
+    public void onResume() {
+        super.onResume();
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                
+                txtEntrada.setText("");
+                txtSaida.setText("");
             }
 
             @Override
@@ -29,7 +42,83 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        txtEntrada.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                switch((int) spinner.getSelectedItemId()){
+                    case 0 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.polegadasCentimetros(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 1 :
+
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.centimetrosPolegadas(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 2 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.pesMetros(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 3 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.metrosPes(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 4 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.milhaKm(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 5 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.kmMilha(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 6 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.fahrenheitCelsius(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 7 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.celsiusFahrenheit(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 8 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.nosKm(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                    case 9 :
+                        if(!txtEntrada.getText().toString().isEmpty())
+                        txtSaida.setText(String.valueOf(
+                                Conversao.kmNos(
+                                        Double.parseDouble(txtEntrada.getText().toString()))));
+                        break;
+                }
+            }
+        });
     }
-
 }
